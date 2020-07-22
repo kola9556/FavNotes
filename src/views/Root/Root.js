7a6661d0266c593bf532d5ad4b0c67fd9d7b27c9
kola9556 <kurdej.aleksandra@gmail.com>
2020-07-22 11:38:11 +0200

    Feat(Button component and global style): Add Button styled-component and global style
    - Add styles for yellow button
    - Add styles for grey button - using props
    - Add Button.module.scss with font Montserrat
    - Installing node-sass
    - Add props and tenary operator to modify elements  ...${({secondary}) => secondary ? white : blue};
    - Add GlobalStyle from createGlobalStyle  helper function

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
-const Root = () => <h1>Hello Alex!</h1>;
+const Root = () => (
+  <>
+    <GlobalStyle />
+    <h1>Hello Alex!</h1>
+    <Button>Close / Save</Button>
+    <Button secondary>Remove</Button>
+  </>
+);
 
 export default Root;
